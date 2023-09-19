ec0c075e626a24c4bc90034004cd920e8688c3a4
Erik Alvarez <erik@qwips.no>
2023-09-19 15:00:35 +0200

    First commit: Kiota w/ modifications

---
+++ Authentication/GoogleAuthProvider.cs
@@ -0,0 +1,34 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace PointerClientSDK.Authentication
+{
+
+    public class GoogleAuthorizationProvider : IAuthenticationProvider
+    {
+        private string _pointerAppScope { get; set; }
+        private GoogleCredential googleCredential;
+
+        public GoogleAuthorizationProvider(string fileName, string scope = "32555940559.apps.googleusercontent.com")
+        {
+            googleCredential = GoogleCredential.FromFile(fileName);
+            _pointerAppScope = scope;
+        }
+        public GoogleAuthorizationProvider(Stream file, string scope = "32555940559.apps.googleusercontent.com")
+        {
+            googleCredential = GoogleCredential.FromStream(file);
+            _pointerAppScope = scope;
+        }
+        public async Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object>? additionalAuthenticationContext = null, CancellationToken cancellationToken = default)
+        {
+            var token = await googleCredential.CreateScoped(_pointerAppScope).UnderlyingCredential.GetAccessTokenForRequestAsync();
+            var headers = new RequestHeaders
+            {
+                { "Authorization", "Bearer " + token }
+            };
+            request.AddHeaders(headers);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# PointerClientSDK
+
+
+
+## Example
+```
+public class PointerTestApiClient
+{
+    private readonly ITestOutputHelper _output;
+    private readonly GoogleAuthorizationProvider _provider;
+    private readonly HttpClientRequestAdapter _httpAdapter;
+
+    private readonly string tenant = "xample-test";
+    private readonly string key = "p2-xample-production";
+    private readonly string appScope = "32555940559.apps.googleusercontent.com";
+    private readonly string apiUrl = "https://garnes.pointer2.com/api";
+    private readonly string filePath = ".\\wm-googlecredentialfile-00FF00FF00.json";
+
+    public PointerTestApiClient(ITestOutputHelper output)
+    {
+        this._output = output;
+        _provider = new GoogleAuthorizationProvider(filePath, appScope);
+        _httpAdapter = new HttpClientRequestAdapter(_provider);
+        _httpAdapter.BaseUrl = apiUrl;
+    }
+
+    public async Task GetArticles()
+    {
+        var client = new Pointer(_httpAdapter);
+        var articles = await client.XrmDb.V1beta1.Tenants[tenant].Products.GetAsync();
+        _output.WriteLine(articles?.ToString());
+    }
+}
+```
